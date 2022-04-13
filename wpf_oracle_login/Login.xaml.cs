31835fb5106207d9fc60891917681890ea393d82
kumoh2 <kumoh2@naver.com>
2022-04-13 20:17:12 +0900

    Comment: Sign Up test

--- wpf_oracle_login/Login.xaml.cs
+++ wpf_oracle_login/Login.xaml.cs
@@ -10,6 +10,8 @@ namespace wpf_oracle_login
     /// </summary>
     public partial class Login : Window
     {
+        string ConString = "Data Source=localhost;User ID=scott;Password=tiger";
+
         public Login()
         {
             InitializeComponent();
@@ -17,13 +19,13 @@ namespace wpf_oracle_login
 
         private void Login_conn()
         {
-            string ConString = "Data Source=localhost;User ID=scott;Password=tiger";
-            string cmdString = "SELECT PWD FROM Z_USR_MAST_REC" + " " + $"WHERE USR_ID = '{id_textbox.Text}'";
+            string cmdString = "SELECT PWD FROM Z_USR_MAST_REC" + " "
+                            + $"WHERE USR_ID = '{IdTextbox.Text}'";
             using OracleConnection conn = new OracleConnection(ConString);
             OracleCommand cmd = new OracleCommand(cmdString, conn);
             conn.Open();
 
-            if (id_textbox.Text == "" || pw_textbox.Password == "")
+            if (IdTextbox.Text == "" || PwTextbox.Password == "")
             {
                 MessageBox.Show("ID 또는Password를입력하세요...");
                 return;
@@ -32,7 +34,7 @@ namespace wpf_oracle_login
             using OracleDataReader reader = cmd.ExecuteReader();
             if (reader.Read())
             {
-                if (pw_textbox.Password != reader["pwd"].ToString())
+                if (PwTextbox.Password != reader["pwd"].ToString())
                 {
                     MessageBox.Show("Password가맞지않습니다...");
                     return;
@@ -50,9 +52,49 @@ namespace wpf_oracle_login
             testwindow.ShowDialog();
         }
 
+        private void SignUp_conn()
+        {
+            string cmdString = "SELECT PWD FROM Z_USR_MAST_REC" + " " 
+                            + $"WHERE USR_ID = '{IdTextbox.Text}'";
+            using OracleConnection conn = new OracleConnection(ConString);
+            OracleCommand cmd = new OracleCommand(cmdString, conn);
+            conn.Open();
+
+            if (IdTextbox.Text == "" || PwTextbox.Password == "")
+            {
+                MessageBox.Show("ID 또는Password를입력하세요...");
+                return;
+            }
+
+            using OracleDataReader reader = cmd.ExecuteReader();
+            if (reader.Read())
+            {
+                if (PwTextbox.Password != reader["pwd"].ToString())
+                {
+                    MessageBox.Show("Password가맞지않습니다...");
+                    return;
+                }
+            }
+            else
+            {
+                MessageBox.Show("등록되지않은ID 입니다.");
+                return;
+            }
+
+            wpf_oracle_login.SignUp testwindow = new wpf_oracle_login.SignUp();
+            testwindow.ShowDialog(); // 회원가입 중 로그인 창 접근 불가능 (모달 호출)
+        }
+
+
+
         private void Login_Button_Click(object sender, System.Windows.RoutedEventArgs e)
         {
             Login_conn();
+        }
+        
+        private void SignUp_Button_Click(object sender, RoutedEventArgs e)
+        {
+            SignUp_conn();
         }
 
         private void Exit_Button_Click(object sender, System.Windows.RoutedEventArgs e)
@@ -67,5 +109,6 @@ namespace wpf_oracle_login
                 Login_conn();
             }
         }
+
     }
 }
