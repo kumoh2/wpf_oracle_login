232d910d6ba185b55dfd7ad7c4d6a54a96897e68
kumoh2 <kumoh2@icloud.com>
2022-04-13 19:47:55 +0900

    Style: namespace change

--- wpf_oracle_login/Login.xaml.cs
+++ wpf_oracle_login/Login.xaml.cs
@@ -1,18 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using Oracle.ManagedDataAccess.Client;
-using System.Text;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using Oracle.ManagedDataAccess.Client;
 
-namespace WpfApp1
+namespace wpf_oracle_login
 {
     /// <summary>
     /// Page1.xaml에 대한 상호 작용 논리
